b3628f2fdcabbb07df0c962af78e9434b1944180
Kristen Smith <kristennlynne@gmail.com>
2022-03-16 00:32:51 -0700

    returns boolean as Yes or No instead of true or false

---
+++ convert-boolean-values-to-yes-or-no.js
@@ -0,0 +1,12 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    if (bool === true) {
+      return "Yes"
+    } else if (bool === false) {
+      return "No"
+    }
+}
+
+console.log(boolToWord(true)); // "Yes"
+console.log(boolToWord(false)); // "No"
