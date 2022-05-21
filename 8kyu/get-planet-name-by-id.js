031cf8ddbc3bdaccb489230dea3fea0c080fc33f
Kristen Smith <kristennlynne@gmail.com>
2022-05-20 18:06:17 -0700

    added new solution

--- 8kyu/get-planet-name-by-id.js
+++ 8kyu/get-planet-name-by-id.js
@@ -9,20 +9,28 @@ function getPlanetName(id){
     switch(id){
       case 1:
         name = 'Mercury'
+        break;
       case 2:
         name = 'Venus'
+        break;
       case 3:
         name = 'Earth'
+        break;
       case 4:
         name = 'Mars'
+        break;
       case 5:
         name = 'Jupiter'
+        break;
       case 6:
         name = 'Saturn'
+        break;
       case 7:
         name = 'Uranus'
+        break;
       case 8:
         name = 'Neptune'
+        break;
     }
     
     return name;
@@ -30,4 +38,3 @@ function getPlanetName(id){
 
 console.log(getPlanetName(2)); // 'Venus'
 console.log(getPlanetName(5)); // 'Jupiter'
-
