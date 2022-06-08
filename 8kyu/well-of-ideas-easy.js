c377801985d0cd2c012e8b69ba4c65df4b582111
Kristen Smith <kristennlynne@gmail.com>
2022-06-08 11:03:00 -0700

    returns message depending on number of good ideas

---
+++ 8kyu/well-of-ideas-easy.js
@@ -0,0 +1,12 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+    const goodCounter = x.filter(i => i === 'good').length;
+    return goodCounter === 0 ? 'Fail!' : goodCounter > 2 ? 'I smell a series!' : 'Publish!';
+}
+
+console.log(well(['bad', 'bad', 'bad'])); // 'Fail!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad'])); // 'Publish!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])); // 'I smell a series!'
