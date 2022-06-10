ef123572500e0f8867950848b8a6e892098fe198
Kristen Smith <kristennlynne@gmail.com>
2022-06-10 14:59:12 -0700

    checks if the string is a palindrome

---
+++ 8kyu/is-palindrome.js
@@ -0,0 +1,8 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('') ? true : false;
+}
+
+console.log(isPalindrome('kristen')); // false
+console.log(isPalindrome('Racecar')); // true
