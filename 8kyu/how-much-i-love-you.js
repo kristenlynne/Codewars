91149bdef135ffc63f53c51faa3d1a464d8e84b1
Kristen Smith <kristennlynne@gmail.com>
2022-06-16 11:52:02 -0700

    takes in number and returns phrase from an array

---
+++ 8kyu/how-much-i-love-you.js
@@ -0,0 +1,25 @@
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+    let phrases = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+    return phrases[(nbPetals - 1) % phrases.length] 
+}
+
+console.log(howMuchILoveYou(1));  // I love you
+console.log(howMuchILoveYou(2));  // a little
+console.log(howMuchILoveYou(3));  // a lot
+console.log(howMuchILoveYou(4));  // passionately
+console.log(howMuchILoveYou(5));  // madly
+console.log(howMuchILoveYou(6));  // not at all
+console.log(howMuchILoveYou(7));  // I love you
