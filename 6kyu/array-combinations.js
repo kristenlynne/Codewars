863b48978ea2ae826e596d953d24b07ad6e3bcb0
Kristen Smith <kristennlynne@gmail.com>
2022-10-26 12:15:29 -0700

    returns Nth smallest element in an array

--- 6kyu/array-combinations.js
+++ 6kyu/array-combinations.js
@@ -14,3 +14,6 @@ console.log(solve([[1,2],[4],[5,6]]), 4);
 console.log(solve([[1,2],[4,4],[5,6,6]]), 4);
 console.log(solve([[1,2],[3,4],[5,6]]), 8);
 console.log(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]), 72)
+
+// one liner
+const solve = arr => arr.reduce((a,c) => a * new Set(c).size, 1)
