771e907357437c03f96a157c4bf3a46193681aa1
Kristen Smith <kristennlynne@gmail.com>
2022-12-18 16:56:40 -0700

    returns sum of the abs value of each of the nums digits

---
+++ 7kyu/summing-a-numbers-digits.js
@@ -0,0 +1,18 @@
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(n) {
+  return Math.abs(n).toString().split('').map(Number).reduce((acc, c) => acc + c, 0);
+} 
+
+console.log(sumDigits(10), 1);
+console.log(sumDigits(99), 18);
+console.log(sumDigits(-32), 5);
+console.log(sumDigits(111), 3);
